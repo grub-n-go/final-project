c715c752f3730bfebba9dea5d2d806bfd847d255
karlp1998 <karl4@hawaii.edu>
2021-04-13 20:26:38 -1000

    Added cursive font for navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -22,13 +22,13 @@ class NavBar extends React.Component {
           ) : ''}
           {/* eslint-disable-next-line max-len */}
           <Menu.Item as={NavLink} id="profilesMenuItem" activeClassName="active" exact to="/profiles" key='profiles'>
-            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 800, fontSize: '24px' }}>Venues</span></Menu.Item>
+            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 1000, fontSize: '28px' }}>Venues</span></Menu.Item>
           <Menu.Item as={NavLink} id="projectsMenuItem" activeClassName="active" exact to="/projects" key='projects'>
-            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 800, fontSize: '24px' }}>Todays Menu</span></Menu.Item>
+            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 1000, fontSize: '28px' }}>Todays Menu</span></Menu.Item>
           <Menu.Item as={NavLink} id="interestsMenuItem" activeClassName="active" exact to="/interests" key='interests'>
-            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 800, fontSize: '24px' }}>Top Picks</span></Menu.Item>
+            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 1000, fontSize: '28px' }}>Top Picks</span></Menu.Item>
           <Menu.Item>
-            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 800, fontSize: '14px' }}>
+            <span className='nav-bar-bowfolio-green' style={{ fontWeight: 1000, fontSize: '14px' }}>
               <Input icon='search' placeholder='Search...' />
             </span>
           </Menu.Item>
