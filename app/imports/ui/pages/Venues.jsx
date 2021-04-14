4ab84822e358505d6c737e2cdea77cf72807e394
ryanv048 <64054663+ryanv048@users.noreply.github.com>
2021-04-14 09:45:23 -1000

    need to continue to fix

--- app/imports/ui/pages/Venues.jsx
+++ app/imports/ui/pages/Venues.jsx
@@ -21,26 +21,28 @@ function getProfileData(email) {
 
 /** Component for layout out a Profile Card. */
 const MakeCard = (props) => (
-  <Card>
-    <Card.Content>
-      <Image floated='right' size='mini' src={props.profile.picture} />
-      <Card.Header>{props.profile.firstName} {props.profile.lastName}</Card.Header>
-      <Card.Meta>
-        <span className='date'>{props.profile.title}</span>
-      </Card.Meta>
-      <Card.Description>
-        {props.profile.bio}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      {_.map(props.profile.interests,
-        (interest, index) => <Label key={index} size='tiny' color='teal'>{interest}</Label>)}
-    </Card.Content>
-    <Card.Content extra>
-      <Header as='h5'>Projects</Header>
-      {_.map(props.profile.projects, (project, index) => <Image key={index} size='mini' src={project}/>)}
-    </Card.Content>
-  </Card>
+  <div className='welcome-background'>
+    <Card>
+      <Card.Content>
+        <Image floated='right' size='mini' src={props.profile.picture} />
+        <Card.Header>{props.profile.firstName} {props.profile.lastName}</Card.Header>
+        <Card.Meta>
+          <span className='date'>{props.profile.title}</span>
+        </Card.Meta>
+        <Card.Description>
+          {props.profile.bio}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        {_.map(props.profile.interests,
+          (interest, index) => <Label key={index} size='tiny' color='teal'>{interest}</Label>)}
+      </Card.Content>
+      <Card.Content extra>
+        <Header as='h5'>Projects</Header>
+        {_.map(props.profile.projects, (project, index) => <Image key={index} size='mini' src={project}/>)}
+      </Card.Content>
+    </Card>
+  </div>
 );
 
 MakeCard.propTypes = {
