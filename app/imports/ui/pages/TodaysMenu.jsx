c0c98371f27ba6038731cbc159f1024ad5ad2aaa
michaelrpierce <58806436+michaelrpierce@users.noreply.github.com>
2021-04-27 05:28:33 -1000

    Created Simple UI Mockup for issue-55

--- app/imports/ui/pages/TodaysMenu.jsx
+++ app/imports/ui/pages/TodaysMenu.jsx
@@ -8,45 +8,66 @@ import { Profiles } from '../../api/profiles/Profiles';
 import { ProfilesProjects } from '../../api/profiles/ProfilesProjects';
 import { Projects } from '../../api/projects/Projects';
 import { ProjectsInterests } from '../../api/projects/ProjectsInterests';
+import { Vendors } from '../../api/vendor/Vendors';
+import { VendorTypes } from '../../api/vendor/VendorTypes';
 
-/** Gets the Project data as well as Profiles and Interests associated with the passed Project name. */
-function getProjectData(name) {
-  const data = Projects.collection.findOne({ name });
-  const interests = _.pluck(ProjectsInterests.collection.find({ project: name }).fetch(), 'interest');
-  const profiles = _.pluck(ProfilesProjects.collection.find({ project: name }).fetch(), 'profile');
-  const profilePictures = profiles.map(profile => Profiles.collection.findOne({ email: profile }).picture);
-  return _.extend({ }, data, { interests, participants: profilePictures });
+// /** Gets the Project data as well as Profiles and Interests associated with the passed Project name. */
+// function getProjectData(name) {
+//   const data = Projects.collection.findOne({ name });
+//   const interests = _.pluck(ProjectsInterests.collection.find({ project: name }).fetch(), 'interest');
+//   const profiles = _.pluck(ProfilesProjects.collection.find({ project: name }).fetch(), 'profile');
+//   const profilePictures = profiles.map(profile => Profiles.collection.findOne({ email: profile }).picture);
+//   return _.extend({ }, data, { interests, participants: profilePictures });
+// }
+
+/** Information regarding todays picks*/
+function getBreakfastData (){
+  const mealName = 'Breakfast';
+  const mealType = 'Chinese';
+  const mealPicture = 'images/orangechicken.jpg';
+  const vendors = _.pluck(VendorTypes.collection.find({vendorType: mealType}).fetch(),'vendor');
+  const vendorsPictures = vendors.map(vendor => Vendors.collection.findOne({email: vendor}).picture);
+  return {mealName, mealType, mealPicture, vendorsPictures};
+}
+function getLunchData (){
+  const mealName = 'Lunch';
+  const mealType = 'Chinese';
+  const mealPicture = 'images/orangechicken.jpg';
+  const vendors = _.pluck(VendorTypes.collection.find({vendorType: mealType}).fetch(),'vendor');
+  const vendorsPictures = vendors.map(vendor => Vendors.collection.findOne({email: vendor}).picture);
+  return {mealName, mealType, mealPicture, vendorsPictures};
+}
+function getDinnerData (){
+  const mealName = 'Dinner';
+  const mealType = 'Chinese';
+  const mealPicture = 'images/orangechicken.jpg';
+  const vendors = _.pluck(VendorTypes.collection.find({vendorType: mealType}).fetch(),'vendor');
+  const vendorsPictures = vendors.map(vendor => Vendors.collection.findOne({email: vendor}).picture);
+  return {mealName, mealType, mealPicture, vendorsPictures};
 }
 
-/** Component for layout out a Project Card. */
+/** Component for layout out a meal Card. */
 const MakeCard = (props) => (
   <Card>
     <Card.Content>
-      <Image floated='left' avatar src={props.project.picture}/>
-      <Card.Header style={{ marginTop: '0px' }}>{props.project.name}</Card.Header>
-      <Card.Meta>
-        <span className='date'>{props.project.title}</span>
-      </Card.Meta>
-      <Card.Description>
-        {props.project.description}
-      </Card.Description>
+      <Card.Header textAlign='center' style={{ marginTop: '0px' }}>{props.mealInfo.mealName}</Card.Header>
+    <Container textAlign='center'>{props.mealInfo.mealType} </Container>
+    </Card.Content>
+    <Card.Content>
+      <Image circular size='large' src={props.mealInfo.mealPicture}/>
     </Card.Content>
     <Card.Content extra>
-      {_.map(props.project.interests,
-        (interest, index) => <Label key={index} size='tiny' color='teal'>{interest}</Label>)}
-    </Card.Content>
-    <Card.Content extra>
-      {_.map(props.project.participants, (p, index) => <Image key={index} circular size='mini' src={p}/>)}
+      {_.map(props.mealInfo.vendorsPictures, (p, index) => <Image key={index} circular size='mini' src={p}/>)}
     </Card.Content>
   </Card>
 );
-
+//{_.map(props.mealInfo.vendorsPictures, (p, index) => <Image key={index} circular size='mini' src={p}/>)}
 MakeCard.propTypes = {
-  project: PropTypes.object.isRequired,
+  mealInfo: PropTypes.object.isRequired,
 };
 
-/** Renders the Project Collection as a set of Cards. */
-class ProjectsPage extends React.Component {
+/** Renders Today's Menu as a set of Cards. */
+class TodaysMenuPage extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -55,13 +76,16 @@ class ProjectsPage extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
-    const projects = _.pluck(Projects.collection.find().fetch(), 'name');
-    const projectData = projects.map(project => getProjectData(project));
+    const breakfastInfo = getBreakfastData();
+    const lunchInfo = getLunchData();
+    const dinnerInfo = getDinnerData();
     return (
       <div className='welcome-background' style={{ paddingTop: '20px' }}>
         <Container id="projects-page">
           <Card.Group>
-            {_.map(projectData, (project, index) => <MakeCard key={index} project={project}/>)}
+            <MakeCard mealInfo={breakfastInfo}/>
+            <MakeCard mealInfo={lunchInfo}/>
+            <MakeCard mealInfo={dinnerInfo}/>
           </Card.Group>
         </Container>
         <div className='green-gradient' style={{ paddingTop: '100px' }}/>
@@ -71,7 +95,7 @@ class ProjectsPage extends React.Component {
   }
 }
 
-ProjectsPage.propTypes = {
+TodaysMenuPage.propTypes = {
   ready: PropTypes.bool.isRequired,
 };
 
@@ -82,7 +106,9 @@ export default withTracker(() => {
   const sub2 = Meteor.subscribe(Projects.userPublicationName);
   const sub3 = Meteor.subscribe(ProjectsInterests.userPublicationName);
   const sub4 = Meteor.subscribe(Profiles.userPublicationName);
+  const sub5 = Meteor.subscribe(Vendors.userPublicationName);
+  const sub6 = Meteor.subscribe(VendorTypes.userPublicationName);
   return {
-    ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready(),
+    ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready() && sub5.ready() && sub6.ready(),
   };
-})(ProjectsPage);
+})(TodaysMenuPage);
