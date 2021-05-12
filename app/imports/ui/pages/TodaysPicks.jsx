40b9f614005f2a453df8685ef8d38077b83dc186
ryanv048 <64054663+ryanv048@users.noreply.github.com>
2021-05-11 18:53:49 -1000

    TopPick is up and running

--- app/imports/ui/pages/TodaysPicks.jsx
+++ app/imports/ui/pages/TodaysPicks.jsx
@@ -5,37 +5,32 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import { VendorClass } from '../../api/interests/vendorClassifications';
-import { Profiles } from '../../api/profiles/Profiles';
-import { ProfilesInterests } from '../../api/profiles/ProfilesInterests';
-import { ProfilesProjects } from '../../api/profiles/ProfilesProjects';
-import { Projects } from '../../api/projects/Projects';
-import { ProjectsInterests } from '../../api/projects/ProjectsInterests';
+import { Vendors } from '../../api/vendor/Vendors';
+import { VendorTypes } from '../../api/vendor/VendorTypes';
 
 /** Returns the Profiles and Projects associated with the passed Interest. */
 function getInterestData(name) {
-  const profiles = _.pluck(ProfilesInterests.collection.find({ interest: name }).fetch(), 'profile');
-  const profilePictures = profiles.map(profile => Profiles.collection.findOne({ email: profile }).picture);
-  const projects = _.pluck(ProjectsInterests.collection.find({ interest: name }).fetch(), 'project');
-  const projectPictures = projects.map(project => Projects.collection.findOne({ name: project }).picture);
+  const vendors = _.pluck(VendorTypes.collection.find({ vendorType: name }).fetch(), 'vendor');
+  const vendorPictures = vendors.map(vendor => Vendors.collection.findOne({ email: vendor }).picture);
   // console.log(_.extend({ }, data, { interests, projects: projectPictures }));
-  return _.extend({ }, { name, profiles: profilePictures, projects: projectPictures });
+  return _.extend({ }, { name, vendors: vendorPictures });
 }
 
 /** Component for layout out an Interest Card. */
 const MakeCard = (props) => (
   <Card>
     <Card.Content>
-      <Card.Header style={{ marginTop: '0px' }}>{props.interest.name}</Card.Header>
+      <Card.Header style={{ marginTop: '0px' }}>{props.vendorType.name}</Card.Header>
     </Card.Content>
     <Card.Content extra>
-      {_.map(props.interest.profiles, (p, index) => <Image key={index} circular size='mini' src={p}/>)}
-      {_.map(props.interest.projects, (p, index) => <Image key={index} circular size='mini' src={p}/>)}
+      {_.map(props.vendorType.vendors, (p, index) => <Image key={index} circular size='mini' src={p}/>)}
+      {_.map(props.vendorType.projects, (p, index) => <Image key={index} circular size='mini' src={p}/>)}
     </Card.Content>
   </Card>
 );
 
 MakeCard.propTypes = {
-  interest: PropTypes.object.isRequired,
+  vendorType: PropTypes.object.isRequired,
 };
 
 /** Renders the Interests as a set of Cards. */
@@ -48,13 +43,13 @@ class InterestsPage extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
-    const interests = _.pluck(VendorClass.collection.find().fetch(), 'vendor');
-    const interestData = interests.map(interest => getInterestData(interest));
+    const vendorTypes = _.pluck(VendorClass.collection.find().fetch(), 'vendor');
+    const vendorTypeData = vendorTypes.map(vendorType => getInterestData(vendorType));
     return (
       <div className='pages-background' style={{ paddingTop: '20px' }}>
         <Container id="interests-page">
           <Card.Group>
-            {_.map(interestData, (interest, index) => <MakeCard key={index} interest={interest}/>)}
+            {_.map(vendorTypeData, (vendorType, index) => <MakeCard key={index} vendorType={vendorType}/>)}
           </Card.Group>
         </Container>
         <div className='green-gradient' style={{ paddingTop: '100px' }}/>
@@ -71,13 +66,10 @@ InterestsPage.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Ensure that minimongo is populated with all collections prior to running render().
-  const sub1 = Meteor.subscribe(ProfilesProjects.userPublicationName);
-  const sub2 = Meteor.subscribe(Projects.userPublicationName);
-  const sub3 = Meteor.subscribe(ProjectsInterests.userPublicationName);
-  const sub4 = Meteor.subscribe(Profiles.userPublicationName);
-  const sub5 = Meteor.subscribe(VendorClass.userPublicationName);
-  const sub6 = Meteor.subscribe(ProfilesInterests.userPublicationName);
+  const sub1 = Meteor.subscribe(VendorClass.userPublicationName);
+  const sub2 = Meteor.subscribe(Vendors.userPublicationName);
+  const sub3 = Meteor.subscribe(VendorTypes.userPublicationName);
   return {
-    ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready() && sub5.ready() && sub6.ready(),
+    ready: sub1.ready() && sub2.ready() && sub3.ready(),
   };
 })(InterestsPage);
